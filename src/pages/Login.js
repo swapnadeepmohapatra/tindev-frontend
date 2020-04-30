a22266fd9a8c186c63ef27165c23f7726cf76f76
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-30 16:35:53 +0530

    added login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,6 +6,15 @@ const Login = () => {
 	return (
 		<div className="login-container" style={{ display: 'flex', flexDirection: 'column' }}>
 			<Navbar />
+			<div className="login-container">
+				<form>
+					{/* <h3>Coding Partner</h3> */}
+					<h3>Find Your Coding Partner</h3>
+					<p>Find the perfect match for your code</p>
+					<input placeholder="GitHub username" />
+					<button type="submit">Send</button>
+				</form>
+			</div>
 		</div>
 	);
 };
