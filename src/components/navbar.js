d56ef946ca09b1048b08ac15462e036bf73eed03
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-30 23:33:25 +0530

    added no more devs in main

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
 	return (
 		<nav className="navMain">
 			<div className="nav-wrapper">
 				<a href="#" className="brand-logo center">
-					<img style={{ height: 50, marginTop: 10 }} src={require('../tinderLogo.png')} alt="Logo" />
+					<Link to="/">
+						<img style={{ height: 50, marginTop: 10 }} src={require('../tinderLogo.png')} alt="Logo" />
+					</Link>
 				</a>
 			</div>
 		</nav>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 
 const Main = ({ match }) => {
-	return <div>{match.params.id}</div>;
+	return (
+		<>
+			<div className="main-container" style={{ display: 'flex', flexDirection: 'column' }}>
+				<Navbar />
+				<div className="main-container" style={{ display: 'flex', flexDirection: 'column' }}>
+					{true && (
+						<>
+							<div className="oops">OOPS!</div>
+							<div className="empty">No more devs! :(</div>
+							<div className="empty" style={{ fontWeight: 'normal' }}>
+								There's no one new around you.
+							</div>
+						</>
+					)}
+					{false && (
+						<>
+							<div className="oops">OOPS!</div>
+							<div className="empty">No more devs! :(</div>
+							<div className="empty" style={{ fontWeight: 'normal' }}>
+								There's no one new around you.
+							</div>
+						</>
+					)}
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default Main;
