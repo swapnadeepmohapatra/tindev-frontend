385ece6ddfe0c5eafccdfa78a5faf1d20412adc7
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-30 11:26:35 +0530

    added materialize css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<div style={{ backgroundColor: '#c1c1c1', display: 'flex' }}>
+			<h1>Hello Dear</h1>
+		</div>
+	);
 };
 
 export default Login;
